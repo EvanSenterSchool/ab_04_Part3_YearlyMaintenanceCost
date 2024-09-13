b6e1c5522925293e6a6e27a3feaeafd1d6e660bc
Evan Senter (614925) <614925@swl.local>
2024-09-13 08:55:38 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double springCost;
+        double summerCost;
+        double fallCost;
+        double winterCost;
+        double finalCost;
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the cost of maintenance in the season of spring.");
+        springCost = scan.nextDouble();
+
+        System.out.println("What is the cost of maintenance in the season of summer.");
+        summerCost = scan.nextDouble();
+
+        System.out.println("What is the cost of maintenance in the season of fall.");
+        fallCost = scan.nextDouble();
+
+        System.out.println("What is the cost of maintenance in the season of winter.");
+        winterCost = scan.nextDouble();
+
+        finalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The Yearly cost for maintenance is " + finalCost);
+
     }
 }
